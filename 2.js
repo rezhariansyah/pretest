af3b8ec84b4b77ed3e575a6ee807aa8d98936a37
Rezha Riansyah R <42859545+rezhariansyah@users.noreply.github.com>
2019-06-16 12:55:10 +0700

    Update 2.js

--- 2.js
+++ 2.js
@@ -1,13 +1,32 @@
-var primaSegitiga = () => {
-    var angka = 5;
-    var output="";
-    for (var i = 1; i <= angka; i++) {
-        for (var j = 1; j <= i; j++) {
-            output+=j + "     ";
+var prima = []
+var segitiga = ""
+
+
+var primaSegitiga = (input) => {
+    if (input <= 0 || input >= 10) {
+        input = "alas/tinggi tidak sesuai"
+    } else {
+        for (var i=1 ; i<=50 ; i++) {
+            t = 0
+            for (var j=1 ; j<=i ; j++) {
+                if(i%j == 0) {
+                    t++
+                }
+            }
+            if(t == 2) {
+                prima.push(i)
+            }
         }
-        console.log(output);
-        output="";
+        // segitiganya
+        for (var i=0 ; i<input ; i++) {
+            for (var j=0 ; j<=i ; j++) {
+                segitiga += prima[j]
+            }
+            segitiga += "\n"
+        }
     }
+
+    return segitiga
 }
 
-console.log(primaSegitiga())
+console.log(primaSegitiga(7))
